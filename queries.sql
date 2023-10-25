0f11cea93d72617b5608a774068df3b9a6cf8926
Wesley Stepler <pwesleystep@outlook.com>
2023-10-25 15:53:53 -0400

    Create Table queries work now

--- queries.sql
+++ queries.sql
@@ -9,13 +9,13 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Post (
-    user_name VARCHAR(50),
     post_id INT AUTO_INCREMENT PRIMARY KEY,
     category VARCHAR(30),
     time_posted DATETIME,
     likes INT,
     post_text VARCHAR(280),
-    FOREIGN KEY (user_name) REFERENCES User(user_name)
+    user_name VARCHAR(50),
+    FOREIGN KEY (user_name) REFERENCES Users(user_name)
 );
 
 CREATE TABLE Comment (
@@ -24,7 +24,7 @@ CREATE TABLE Comment (
     user_name VARCHAR(50),
     comment_text VARCHAR(280),
     FOREIGN KEY (post_id) REFERENCES Post(post_id),
-    FOREIGN KEY (user_name) REFERENCES User(user_name)
+    FOREIGN KEY (user_name) REFERENCES Users(user_name)
 );
 
 CREATE TABLE Post_Comments (
@@ -37,23 +37,23 @@ CREATE TABLE Post_Comments (
 CREATE TABLE Is_Friends_With (
     user_name1 VARCHAR(50),
     user_name2 VARCHAR(50),
-    FOREIGN KEY (user_name1) REFERENCES User(user_name),
-    FOREIGN KEY (user_name2) REFERENCES User(user_name),
+    FOREIGN KEY (user_name1) REFERENCES Users(user_name),
+    FOREIGN KEY (user_name2) REFERENCES Users(user_name),
     PRIMARY KEY (user_name1, user_name2)
 );
 
 CREATE TABLE Sends_Request_To (
     user_name1 VARCHAR(50),
     user_name2 VARCHAR(50),
-    FOREIGN KEY (user_name1) REFERENCES User(user_name),
-    FOREIGN KEY (user_name2) REFERENCES User(user_name),
+    FOREIGN KEY (user_name1) REFERENCES Users(user_name),
+    FOREIGN KEY (user_name2) REFERENCES Users(user_name),
     PRIMARY KEY (user_name1, user_name2)
 );
 
 CREATE TABLE Likes_Comment (
     user_name VARCHAR(50),
     comment_id INT,
-    FOREIGN KEY (user_name) REFERENCES User(user_name),
+    FOREIGN KEY (user_name) REFERENCES Users(user_name),
     FOREIGN KEY (comment_id) REFERENCES Comment(comment_id),
     PRIMARY KEY (user_name, comment_id)
 );
@@ -61,18 +61,18 @@ CREATE TABLE Likes_Comment (
 CREATE TABLE Likes_Post (
     user_name VARCHAR(50),
     post_id INT,
-    FOREIGN KEY (user_name) REFERENCES User(user_name),
+    FOREIGN KEY (user_name) REFERENCES Users(user_name),
     FOREIGN KEY (post_id) REFERENCES Post(post_id),
     PRIMARY KEY (user_name, post_id)
 );
 
 CREATE TABLE Replies(
     comment_id INT,
-    reply_num INT AUTO_INCREMENT,
+    reply_num INT,
     user_name VARCHAR(50),
-    timestamp VARCHAR(16),
+    time_stamp VARCHAR(16),
     FOREIGN KEY (comment_id) REFERENCES Comment(comment_id),
-    FOREIGN KEY (user_name) REFERENCES User(user_name),
+    FOREIGN KEY (user_name) REFERENCES Users(user_name),
     PRIMARY KEY (comment_id, reply_num)
 );
 
