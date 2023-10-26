952da5e201a184b90e354cc8cdf72846debfd862
Wesley Stepler <pwesleystep@outlook.com>
2023-10-26 12:05:04 -0400

    Hardcoded DB login credentials, will fix later

--- connect_db.py
+++ connect_db.py
@@ -6,17 +6,13 @@ import queries
 try:
     connection = connect(
         host="mysql01.cs.virginia.edu",
-        user=input("Enter username: "),
-        password=getpass("Enter password: "),
+        user="pws3ms",
+        password="Fall2023",
         database="pws3ms_c",
     )
 
 except Error as e:
     print(e)
 
-query = queries.create_user_table
-with connection.cursor() as cursor:
-    cursor.execute(query)
-    connection.commit()
     
 
