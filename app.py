cfe52dedb772668b01331ce70726043eb13c248b
Wesley Stepler <pwesleystep@outlook.com>
2023-11-01 11:16:25 -0400

    Added some additional documentation

--- app.py
+++ app.py
@@ -3,20 +3,25 @@ from datetime import timedelta
 import connect_db
 
 app = Flask(__name__)
-app.secret_key = "hello"
-#Store session data for one week
+app.secret_key = "hello" # This can be anything (and maybe should be more secure), I was just following my boy Tim's tutorial.
+
+# Store session data for one week
 app.permanent_session_lifetime = timedelta(days=7)
 
 @app.route('/', methods=["POST", "GET"])
 def login():
 
-    #SECURITY UPDATES NEEDED, ESPECIALLY FOR PASSWORD STORING AND CHECKING
+    # SECURITY UPDATES NEEDED, ESPECIALLY FOR PASSWORD STORING AND CHECKING
+
+    # Get the username and password from the front end.
     if request.method == "POST":
         session.permanent = True
         user = request.form["usr"]
         password = request.form["pwd"]
 
-        #Make sure this username and password exist together in some user instance
+        # Make sure this username and password exist together in some user instance.
+        # If not, redirect user to bad login page, which will ask either for different credentials
+        # or for the user to create a new account.
         validate_request = connect_db.get_all_users()
         for user_creds in validate_request:
             if user in user_creds and password in user_creds:
@@ -25,12 +30,14 @@ def login():
         return redirect(url_for("bad_login"))
     
     else:
+        # If there are saved login credentials, just go to the user's page.
         if "user" in session:
             return redirect(url_for("user"))
         return render_template('home.html')
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
+    # Get new user credentials from the front end.
     if request.method == "POST":
         session.permanent = True
         user = request.form["usr"]
@@ -41,6 +48,7 @@ def signup():
         session["user"] = user
         email = request.form["email"]
 
+        # Execute the SQL Query to add a new user with the above arguments
         connect_db.add_new_user(user, password, first_name, last_name, email)
 
         return redirect(url_for("user"))
@@ -55,6 +63,7 @@ def bad_login():
 
 @app.route("/user")
 def user():
+   # Very basic user page. This will eventually be the user's home page.
    if "user" in session:
        user = session["user"]
        return f"<h1>{user}</h1>" 
@@ -63,6 +72,7 @@ def user():
    
 @app.route("/logout")
 def logout():
+    # Currently no page or button for this.
     session.pop("user", None)
     return redirect(url_for("login"))
 
